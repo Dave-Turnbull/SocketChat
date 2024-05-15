4d88835ed29a29ac0ba37fcbb44d10c6e4c80d96
dave-turnbull <dav-tu@hotmail.com>
2024-05-15 14:12:10 +0100

    recommiting last commit, added user input

--- Socket Chat Frontend/src/App.jsx
+++ Socket Chat Frontend/src/App.jsx
@@ -1,9 +1,40 @@
+import { useState } from 'react';
 import './App.css'
+import {io} from 'socket.io-client'
 
 function App() {
+  const [userInput, setUserInput] = useState('')
+  const socket = io('localhost:3000')
+
+  //event listener for when client is connected to socket
+  socket.on("connect", () => {
+    console.log(socket.id, 'connected');
+  });
+  //event listener for when client is disconnected
+  socket.on("disconnect", () => {
+    console.log(socket.id, 'disconnected');
+  });
+
+  //event listener for when client recieves an event called 'message'
+  socket.on('message', text => {
+      console.log(text)
+  });
+
+  //handling user input
+  const handleInput = (e) => {
+    setUserInput(e.target.value)
+  }
+
+  //handling button click, send userInput
+  const handleButtonClick = () => {
+    socket.send(userInput)
+    setUserInput('')
+  }
 
   return (
     <>
+      <input onChange={handleInput} value={userInput}></input>
+      <button onClick={handleButtonClick}>Send Message</button>
     </>
   )
 }
